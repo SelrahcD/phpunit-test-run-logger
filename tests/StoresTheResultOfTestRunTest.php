86c0adec2d2ce3f1db6941900fc27c029dcff4f4
SelrahcD <c.desneuf@gmail.com>
2022-07-21 10:19:36 +0200

    Inline method

--- tests/StoresTheResultOfTestRunTest.php
+++ tests/StoresTheResultOfTestRunTest.php
@@ -15,33 +15,36 @@ class StoresTheResultOfTestRunTest extends TestCase
     * @test
     */
     public function when_passing_in_a_plain_text_file(): void {
-        $this->assertTheCorrectFileIsCreated('SinglePassingTest');
+        $fixture = 'SinglePassingTest';
+
+        $this->runATestFile($fixture);
+
+        $this->assertFileEquals(__DIR__ . '/Fixtures/' . $fixture . '/expected_logs', __DIR__ . '/test_run_logs');
+
     }
 
     /**
      * @test
      */
     public function when_failing_in_a_plain_text_file(): void {
-        $this->assertTheCorrectFileIsCreated('SingleFailingTest');
+        $fixture = 'SingleFailingTest';
+
+        $this->runATestFile($fixture);
+
+        $this->assertFileEquals(__DIR__ . '/Fixtures/' . $fixture . '/expected_logs', __DIR__ . '/test_run_logs');
     }
 
     /**
      * @test
      */
     public function when_one_test_is_failing_in_a_plain_text_file(): void {
-        $this->assertTheCorrectFileIsCreated('MultipleTests');
-    }
+        $fixture = 'MultipleTests';
 
-    /**
-     * @param string $fixture
-     * @return void
-     */
-    private function assertTheCorrectFileIsCreated(string $fixture): void
-    {
         $this->runATestFile($fixture);
 
         $this->assertFileEquals(__DIR__ . '/Fixtures/' . $fixture . '/expected_logs', __DIR__ . '/test_run_logs');
     }
+
 
     /**
      * @param string $fixture
