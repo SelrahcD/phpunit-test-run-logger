a441baaa3fe01f42fcc6ba72ab2e9b12253a5100
SelrahcD <c.desneuf@gmail.com>
2022-07-21 10:10:49 +0200

    Fix test name

--- tests/Fixtures/SingleFailingTest/SomeTest.php
+++ tests/Fixtures/SingleFailingTest/SomeTest.php
@@ -9,9 +9,14 @@ use PHPUnit\Framework\TestCase;
 class SomeTest extends TestCase
 {
 
-    public function test_it_passes(): void
+    public function test_it_fails(): void
     {
         // @phpstan-ignore-next-line
         $this->assertTrue(false);
+    }
+
+    public function test_it_passes(): void
+    {
+        $this->assertTrue(true);
     }
 }
